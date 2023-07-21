5cee699da1f9027c723b10c1edf43318c232fc85
Bella Ball <mohawkball@gmail.com>
2023-07-21 00:14:15 -0500

    settings and bg color

--- first_file.py
+++ first_file.py
@@ -1,21 +1,29 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
+    #initialize 
     pygame.init()
-    #make the background - size is 1560 x 800
-    screen = pygame.display.set_mode((1560,800))
+    #make the background
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((
+        ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion!")
 
+    
+    
     #game main loop
     while True:
         
         #watch for action
         for event in pygame.event.get():
-
             #close game
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        #redrawing the screen
+        screen.fill(ai_settings.bg_color)
             
         #updates screen
         pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+#class for game settings
+class Settings():
+
+    def __init__(self):
+        #initialize
+        #screen settings
+        self.screen_width = 1560
+        self.screen_height = 800
+        self.bg.color - (230, 230, 230)
