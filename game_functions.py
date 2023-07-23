ab0c3f3a3655da38b19679fca8f75dfd0279a261
Bella Ball <mohawkball@gmail.com>
2023-07-23 15:27:21 -0500

    refactor update bullets

--- game_functions.py
+++ game_functions.py
@@ -55,8 +55,11 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
-    #when clear, empty bullets new fleet
     if len(aliens) == 0:
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
