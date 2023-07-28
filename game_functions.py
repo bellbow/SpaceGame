ac6079795937f5c7a043cf5188567b3daac4c85a
Bella Ball <mohawkball@gmail.com>
2023-07-28 00:43:01 -0500

    final commit

--- game_functions.py
+++ game_functions.py
@@ -27,7 +27,7 @@ def check_keyup_events(event, ship):
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets):
     #respond to events
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -38,15 +38,20 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
                 check_keyup_events(event,ship)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
+                check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
         ai_settings.initialize_dynamic_settings()
         pygame.mouse.set_visible(False)
         stats.reset_stats()
         stats.game_active = True
+
+        sb.prep_score()
+        sb.prep_high_score()
+        sb.prep_level()
+        sb.prep_ships()
 
         aliens.empty()
         bullets.empty()
@@ -97,9 +102,11 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
+
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
-
-
 
 
 
@@ -149,11 +156,31 @@ def change_fleet_direction(ai_settings, aliens):
     ai_settings.fleet_direction *= -1
 
 
+
+def check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets):
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            ship_hit(ai_settings, screen, stats, ship, aliens, bullets)
+            break
+
+def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
+    #check for edge then update aliens
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+    if pygame.sprite.spritecollideany(ship, aliens):
+        ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
+
+    check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
+
 #collisions
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
     #reduce ships left
     if stats.ships_left > 0:
         stats.ships_left -= 1
+
+        sb.prep_ships()
 
         #empty aliens and bullets
         aliens.empty()
@@ -168,22 +195,6 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     else:
         stats.game_active = False
         pygame.mouse.set_visible(True)
-
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
-    screen_rect = screen.get_rect()
-    for alien in aliens.sprites():
-        if alien.rect.bottom >= screen_rect.bottom:
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
-            break
-
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
-    #check for edge then update aliens
-    check_fleet_edges(ai_settings, aliens)
-    aliens.update()
-
-    if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
-    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
 def check_high_score(stats, sb):
     #check for new high score
--- ship.py
+++ ship.py
@@ -1,9 +1,10 @@
 import pygame
-
-class Ship():
+from pygame.sprite import Sprite
+class Ship(Sprite):
 
     def __init__(self,ai_settings,screen):
         #initialize ship
+        super(Ship,self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
